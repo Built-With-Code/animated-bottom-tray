f19916ced9011adb45f959576652de6f7822327f
Akhil Waghmare <akhilwaghmare@gmail.com>
2024-04-25 16:17:59 -0700

    Starter code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
 "use client";
 
-import Tray from "@/components/Tray";
 import { Button } from "@/components/ui/button";
-import { AnimatePresence } from "framer-motion";
-import { useState } from "react";
 
 export default function Home() {
-  const [trayOpen, setTrayOpen] = useState(false);
-
   return (
     <main className="min-h-screen flex justify-center items-center">
-      <Button onClick={() => setTrayOpen(true)}>Open tray</Button>
-      <AnimatePresence>
-        {trayOpen && <Tray closeTray={() => setTrayOpen(false)} />}
-      </AnimatePresence>
+      <Button>Open tray</Button>
     </main>
   );
 }
